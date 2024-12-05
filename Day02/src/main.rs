6b99a0b71e0e2869a022b9c1e00824d4a092e574
K4R70FF3L <thies.h13@gmail.com>
2024-12-05 15:58:09 +0100

    Solved Day02 part 2

--- Day02/src/main.rs
+++ Day02/src/main.rs
@@ -1,10 +1,19 @@
 use std::fs;
 
-fn main() {
-    run().expect("");
+#[derive(Eq, PartialEq)]
+enum Sorting {
+    Asc,
+    Desc,
 }
 
-fn run() -> Result<(), String> {
+struct Change {
+    amount: i64,
+    direction: Sorting,
+}
+
+const ALLOWED_DEVIATIONS: i64 = 1;
+
+fn main() {
     let input =
         fs::read_to_string("./res/input.txt").expect("Should have been able to read the file");
 
@@ -30,54 +39,112 @@ fn run() -> Result<(), String> {
     println!("Part 2:");
     result = 0;
     for report in reports.iter() {
-        if is_report_safe2(report) {
+        if is_report_safe2(report, 0) {
             result += 1;
         }
     }
     println!("{}", result);
-
-    return Ok(());
 }
 
-fn is_report_safe2(report: &Vec<i64>) -> bool {
-    let direction: &str;
-    if report.get(0) < report.get(1) {
-        direction = "ASC";
-    } else if report.get(0) > report.get(1) {
-        direction = "DESC";
+fn is_report_safe2(report: &Vec<i64>, change_count: i64) -> bool {
+    if change_count > ALLOWED_DEVIATIONS {
+        return false;
+    }
+
+    let changes = report
+        .iter()
+        .enumerate()
+        .filter(|(index, _)| index + 1 < report.len())
+        .map(|(index, item)| Change {
+            amount: (report.get(index + 1).unwrap() - item).abs(),
+            direction: if report.get(index + 1).unwrap() - item > 0 {
+                Sorting::Asc
+            } else {
+                Sorting::Desc
+            },
+        })
+        .collect::<Vec<Change>>();
+
+    let mut asc_balance = 0;
+
+    for (index, change) in changes.iter().enumerate() {
+        if change.amount < 1 || change.amount > 3 {
+            return is_report_safe2(
+                &report
+                    .iter()
+                    .enumerate()
+                    .filter(|(report_index, _)| index + 1 != *report_index)
+                    .map(|(_, item)| *item)
+                    .collect::<Vec<i64>>(),
+                change_count + 1,
+            ) || is_report_safe2(
+                &report
+                    .iter()
+                    .enumerate()
+                    .filter(|(report_index, _)| index != *report_index)
+                    .map(|(_, item)| *item)
+                    .collect::<Vec<i64>>(),
+                change_count + 1,
+            );
+        }
+
+        match change.direction {
+            Sorting::Asc => asc_balance += 1,
+            Sorting::Desc => asc_balance -= 1,
+        }
+    }
+
+    let overall_direction;
+
+    if asc_balance > 0 {
+        overall_direction = Sorting::Asc;
+    } else if asc_balance < 0 {
+        overall_direction = Sorting::Desc;
     } else {
         return false;
     }
-    let mut index = 1;
-    while index < report.len() {
-        let mut difference = report.get(index).unwrap() - report.get(index - 1).unwrap();
-        if direction == "DESC" {
-            difference *= -1;
-        }
 
-        if difference < 1 || difference > 3 {
-            return false;
+    for (index, change) in changes.iter().enumerate() {
+        if overall_direction != change.direction {
+            return is_report_safe2(
+                &report
+                    .iter()
+                    .enumerate()
+                    .filter(|(report_index, _)| index + 1 != *report_index)
+                    .map(|(_, item)| *item)
+                    .collect::<Vec<i64>>(),
+                change_count + 1,
+            ) || is_report_safe2(
+                &report
+                    .iter()
+                    .enumerate()
+                    .filter(|(report_index, _)| index != *report_index)
+                    .map(|(_, item)| *item)
+                    .collect::<Vec<i64>>(),
+                change_count + 1,
+            );
         }
-
-        index += 1;
     }
+
     return true;
 }
 
 fn is_report_safe(report: &Vec<i64>) -> bool {
-    let direction: &str;
+    let direction;
     if report.get(0) < report.get(1) {
-        direction = "ASC";
+        direction = Sorting::Asc;
     } else if report.get(0) > report.get(1) {
-        direction = "DESC";
+        direction = Sorting::Desc;
     } else {
         return false;
     }
     let mut index = 1;
     while index < report.len() {
         let mut difference = report.get(index).unwrap() - report.get(index - 1).unwrap();
-        if direction == "DESC" {
-            difference *= -1;
+
+        match direction {
+            Sorting::Desc => difference *= -1,
+            _ => (),
         }
 
         if difference < 1 || difference > 3 {
